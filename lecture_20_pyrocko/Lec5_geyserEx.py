f1698593caf7fd91df95f32ccdbc8ceb5c8919b9
EvaEibl <eva.ps.eibl@hotmail.com>
2020-08-05 19:11:50 +0200

    edited lecture 14-16

---
+++ lecture_20_pyrocko/Lec5_geyserEx.py
@@ -0,0 +1,41 @@
+# Lecture 5: Snuffler exercise functions 
+# 
+# author: Eva Eibl eva.eibl@uni-potsdam.de
+# SS 2019: Module MGPW02
+# 16.5.2019
+# -----------------------------
+import Lec5_geyserEx_functions as h
+import matplotlib.pyplot as plt
+
+
+# -- import the snuffler marker file, time in julian days --
+h.read_snuffler_marker
+
+
+# -- calculate the waiting time after eruption/ duration of bursts in minutes --  
+
+
+
+# -- calculate mean time --
+
+
+
+# -- plot waiting time after eruptions/ duration of bursts vs. time --
+plot
+
+
+# -- plot histogram of the intereruption times -- 
+hist
+
+
+
+
+
+
+
+
+
+
+
+
+
